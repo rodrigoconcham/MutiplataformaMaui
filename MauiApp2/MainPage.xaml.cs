8cdade5587906e9150c899dc110446cbf78a23b6
Rodrigo Concha <rodrigoconcham@gmail.com>
2023-05-17 10:29:56 -0400

    distinguir entre android y windows

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	//int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        //count++;
+
+        //if (count == 1)
+        //	CounterBtn.Text = $"Clicked {count} time";
+        //else
+        //	CounterBtn.Text = $"Clicked {count} times";
+
+        //SemanticScreenReader.Announce(CounterBtn.Text);
+
+        if (DeviceInfo.Current.Platform == DevicePlatform.Android) 
+        {
+
+              CounterBtn.Text = $"Hola Mundo Android";
+
+        }
+
+
+        if (DeviceInfo.Current.Platform == DevicePlatform.WinUI)
+        {
+
+            CounterBtn.Text = $"Hola Mundo Windows";
+
+
+        }
+
+
+    }
+}
+
